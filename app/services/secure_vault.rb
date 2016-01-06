7543e59ebc9f9c9172c3662f8b9831e933ae1826
Jonathan Garay <jonathan@devmask.net>
2016-01-05 18:23:46 -0700

    added sentinel for failover

--- app/services/secure_vault.rb
+++ app/services/secure_vault.rb
@@ -1,14 +1,17 @@
 class SecureVault
   attr_accessor :redis
 
+  def self.redis
+    Redis.new(port: 26379)
+  end
+
   def self.store(token)
-    redis = Redis.new
     redis.set token.id, token.encrypted_payload
     redis.expire token.id, 10
   end
 
   def self.retrieve(token_id)
-    encrypted_payload = Redis.new.get(token_id)
+    encrypted_payload = redis.get(token_id)
     SecureToken.new id: token_id, encrypted_payload: encrypted_payload if encrypted_payload
   end
 end
