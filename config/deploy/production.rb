c0ca433be40ae5fd1db6b5611698a0c03d4d461e
Jonathan Garay <jonathan@devmask.net>
2016-01-05 17:29:27 -0700

    added defaul secret base

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'miniconekta.devmask.net', user: 'root', roles: %w{app puma_nginx}
+server 'miniconekta.devmask.net', user: 'root', roles: %w{app puma_nginx web}
 # server 'example.com', user: 'deploy', roles: %w{app web},  other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
